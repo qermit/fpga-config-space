612ded88b626d0a3aaccd52e346f21d0a6a4ae8d
Alessandro Rubini <rubini@gnudd.com>
2012-11-26 19:56:48 +0100

    sdbfs/kernel/sdbfs-file: UNTESTED: added write
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- sdbfs/kernel/sdbfs-file.c
+++ sdbfs/kernel/sdbfs-file.c
@@ -13,8 +13,8 @@
 
 #include "sdbfs-int.h"
 
-static ssize_t sdbfs_read(struct file *f, char __user *buf, size_t count,
-			  loff_t *offp)
+static ssize_t sdbfs_read(struct file *f, char __user *buf,
+				  size_t count, loff_t *offp)
 {
 	struct inode *ino = f->f_dentry->d_inode;
 	struct sdbfs_dev *sd;
@@ -59,6 +59,53 @@ static ssize_t sdbfs_read(struct file *f, char __user *buf, size_t count,
 	return done;
 }
 
-const struct file_operations sdbfs_fops = {
+static ssize_t sdbfs_write(struct file *f, const char __user *buf,
+				 size_t count, loff_t *offp)
+{
+	struct inode *ino = f->f_dentry->d_inode;
+	struct sdbfs_dev *sd;
+	struct sdbfs_inode *inode;
+	char kbuf[16];
+	unsigned long start, size;
+	ssize_t i, done;
+
+	inode = container_of(ino, struct sdbfs_inode, ino);
+	sd = inode->sd;
+	start = be64_to_cpu(inode->info.s_d.sdb_component.addr_first);
+	size = be64_to_cpu(inode->info.s_d.sdb_component.addr_last) + 1 - start;
+
+	if (*offp > size)
+		return 0;
+	if (*offp + count > size)
+		count = size - *offp;
+	done = 0;
+	start += *offp;
+	while (count) {
+		/* Horribly inefficient, just copy a few bytes at a time */
+		int n = sizeof(kbuf) > count ? count : sizeof(kbuf);
+
+		if (copy_from_user(kbuf, buf, n))
+			return -EFAULT;
+		/* FIXME: error checking */
+		i = sd->ops->write(sd, start + done, kbuf, n);
+		if (i < 0) {
+			if (done)
+				return done;
+			return i;
+		}
+		buf += i;
+		done += i;
+		count -= i;
+		if (i != n) {
+			/* Partial write: done for this time */
+			break;
+		}
+	}
+	*offp += done;
+	return done;
+}
+
+const struct file_operations sdbfs_iomem_fops = {
 	.read		= sdbfs_read,
+	.write		= sdbfs_write,
 };
